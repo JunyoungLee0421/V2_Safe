eba1232fa0546dc1ab143372be8962a9f040d29c
Junyoung Lee <tim000421@gmail.com>
2024-01-30 15:10:54 -0800

    initial commit

--- index.js
+++ index.js
@@ -30,74 +30,74 @@ const node_session_secret = process.env.NODE_SESSION_SECRET;
 
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 var mongoStore = MongoStore.create({
-	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@cluster0.fuu9a.mongodb.net/sessions`,
-	crypto: {
-		secret: mongodb_session_secret
-	}
+    mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@cluster0.7uvzogm.mongodb.net/sessions`,
+    crypto: {
+        secret: mongodb_session_secret
+    }
 })
 
-app.use(session({ 
+app.use(session({
     secret: node_session_secret,
-	store: mongoStore, //default is memory store 
-	saveUninitialized: false, 
-	resave: true
+    store: mongoStore, //default is memory store 
+    saveUninitialized: false,
+    resave: true
 }
 ));
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.render("index");
 });
 
-app.get('/about', (req,res) => {
+app.get('/about', (req, res) => {
     var color = req.query.color;
     if (!color) {
         color = "black";
     }
 
-    res.render("about", {color: color} );
+    res.render("about", { color: color });
 });
 
-app.get('/contact', (req,res) => {
+app.get('/contact', (req, res) => {
     var missingEmail = req.query.missing;
-    res.render("contact", {missing: missingEmail});
+    res.render("contact", { missing: missingEmail });
 });
 
-app.post('/submitEmail', (req,res) => {
+app.post('/submitEmail', (req, res) => {
     var email = req.body.email;
     if (!email) {
         res.redirect('/contact?missing=1');
     }
     else {
-        res.render("submitEmail", {email: email});
+        res.render("submitEmail", { email: email });
     }
 });
 
-app.get('/createTables', async (req,res) => {
+app.get('/createTables', async (req, res) => {
 
     const create_tables = include('database/create_tables');
 
     var success = create_tables.createTables();
     if (success) {
-        res.render("successMessage", {message: "Created tables."} );
+        res.render("successMessage", { message: "Created tables." });
     }
     else {
-        res.render("errorMessage", {error: "Failed to create tables."} );
+        res.render("errorMessage", { error: "Failed to create tables." });
     }
 });
 
-app.get('/createUser', (req,res) => {
+app.get('/createUser', (req, res) => {
     res.render("createUser");
 });
 
 
-app.get('/login', (req,res) => {
+app.get('/login', (req, res) => {
     res.render("login");
 });
 
-app.post('/submitUser', async (req,res) => {
+app.post('/submitUser', async (req, res) => {
     var username = req.body.username;
     var password = req.body.password;
 
@@ -108,15 +108,15 @@ app.post('/submitUser', async (req,res) => {
     if (success) {
         var results = await db_users.getUsers();
 
-        res.render("submitUser",{users:results});
+        res.render("submitUser", { users: results });
     }
     else {
-        res.render("errorMessage", {error: "Failed to create user."} );
+        res.render("errorMessage", { error: "Failed to create user." });
     }
 
 });
 
-app.post('/loggingin', async (req,res) => {
+app.post('/loggingin', async (req, res) => {
     var username = req.body.username;
     var password = req.body.password;
 
@@ -127,10 +127,10 @@ app.post('/loggingin', async (req,res) => {
         if (results.length == 1) { //there should only be 1 user in the db that matches
             if (bcrypt.compareSync(password, results[0].password)) {
                 req.session.authenticated = true;
-                req.session.user_type = results[0].type; 
+                req.session.user_type = results[0].type;
                 req.session.username = username;
                 req.session.cookie.maxAge = expireTime;
-        
+
                 res.redirect('/loggedIn');
                 return;
             }
@@ -139,9 +139,9 @@ app.post('/loggingin', async (req,res) => {
             }
         }
         else {
-            console.log('invalid number of users matched: '+results.length+" (expected 1).");
+            console.log('invalid number of users matched: ' + results.length + " (expected 1).");
             res.redirect('/login');
-            return;            
+            return;
         }
     }
 
@@ -152,21 +152,21 @@ app.post('/loggingin', async (req,res) => {
 
 
 function isValidSession(req) {
-	if (req.session.authenticated) {
-		return true;
-	}
-	return false;
+    if (req.session.authenticated) {
+        return true;
+    }
+    return false;
 }
 
 function sessionValidation(req, res, next) {
-	if (!isValidSession(req)) {
-		req.session.destroy();
-		res.redirect('/login');
-		return;
-	}
-	else {
-		next();
-	}
+    if (!isValidSession(req)) {
+        req.session.destroy();
+        res.redirect('/login');
+        return;
+    }
+    else {
+        next();
+    }
 }
 
 function isAdmin(req) {
@@ -177,93 +177,93 @@ function isAdmin(req) {
 }
 
 function adminAuthorization(req, res, next) {
-	if (!isAdmin(req)) {
+    if (!isAdmin(req)) {
         res.status(403);
-        res.render("errorMessage", {error: "Not Authorized"});
+        res.render("errorMessage", { error: "Not Authorized" });
         return;
-	}
-	else {
-		next();
-	}
+    }
+    else {
+        next();
+    }
 }
 
 app.use('/loggedin', sessionValidation);
 app.use('/loggedin/admin', adminAuthorization);
 
-app.get('/loggedin', (req,res) => {
+app.get('/loggedin', (req, res) => {
     res.render("loggedin");
 });
 
-app.get('/loggedin/info', (req,res) => {
+app.get('/loggedin/info', (req, res) => {
     res.render("loggedin-info");
 });
 
-app.get('/loggedin/admin', (req,res) => {
+app.get('/loggedin/admin', (req, res) => {
     res.render("admin");
 });
 
-app.get('/loggedin/memberinfo', (req,res) => {
-    res.render("memberInfo", {username: req.session.username, user_type: req.session.user_type});
+app.get('/loggedin/memberinfo', (req, res) => {
+    res.render("memberInfo", { username: req.session.username, user_type: req.session.user_type });
 });
 
 
-app.get('/cat/:id', (req,res) => {
+app.get('/cat/:id', (req, res) => {
     var cat = req.params.id;
 
-    res.render("cat", {cat: cat});
+    res.render("cat", { cat: cat });
 });
 
 
-app.get('/api', (req,res) => {
-	var user = req.session.user;
+app.get('/api', (req, res) => {
+    var user = req.session.user;
     var user_type = req.session.user_type;
-	console.log("api hit ");
+    console.log("api hit ");
 
-	var jsonResponse = {
-		success: false,
-		data: null,
-		date: new Date()
-	};
+    var jsonResponse = {
+        success: false,
+        data: null,
+        date: new Date()
+    };
 
-	
-	if (!isValidSession(req)) {
-		jsonResponse.success = false;
-		res.status(401);  //401 == bad user
-		res.json(jsonResponse);
-		return;
-	}
 
-	if (typeof id === 'undefined') {
-		jsonResponse.success = true;
-		if (user_type === "admin") {
-			jsonResponse.data = ["A","B","C","D"];
-		}
-		else {
-			jsonResponse.data = ["A","B"];
-		}
-	}
-	else {
-		if (!isAdmin(req)) {
-			jsonResponse.success = false;
-			res.status(403);  //403 == good user, but, user should not have access
-			res.json(jsonResponse);
-			return;
-		}
-		jsonResponse.success = true;
-		jsonResponse.data = [id + " - details"];
-	}
+    if (!isValidSession(req)) {
+        jsonResponse.success = false;
+        res.status(401);  //401 == bad user
+        res.json(jsonResponse);
+        return;
+    }
 
-	res.json(jsonResponse);
+    if (typeof id === 'undefined') {
+        jsonResponse.success = true;
+        if (user_type === "admin") {
+            jsonResponse.data = ["A", "B", "C", "D"];
+        }
+        else {
+            jsonResponse.data = ["A", "B"];
+        }
+    }
+    else {
+        if (!isAdmin(req)) {
+            jsonResponse.success = false;
+            res.status(403);  //403 == good user, but, user should not have access
+            res.json(jsonResponse);
+            return;
+        }
+        jsonResponse.success = true;
+        jsonResponse.data = [id + " - details"];
+    }
+
+    res.json(jsonResponse);
 
 });
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("*", (req,res) => {
-	res.status(404);
-	res.render("404");
+app.get("*", (req, res) => {
+    res.status(404);
+    res.render("404");
 })
 
 app.listen(port, () => {
-	console.log("Node application listening on port "+port);
+    console.log("Node application listening on port " + port);
 }); 
